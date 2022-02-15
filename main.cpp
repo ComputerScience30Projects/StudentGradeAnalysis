70c9e889424c0fcc04ebace64fef5a9bbac46cac
Omar <omar.elmanfaluty@gmail.com>
2022-02-15 15:03:23 +0000

    added stats still needs debugging

--- main.cpp
+++ main.cpp
@@ -19,35 +19,106 @@ public:
         _grade = 100;
     }
 
-    ~Student();   
+    ~Student()
+    {
+
+    }   
 
     /*Setters*/
     void setGrade(float grade) { _grade = grade;}
 
     /*Getters*/
-    float getGrade();
-    std::string getName();
+    float getGrade()
+    {
+        return _grade;
+    }
+
+    std::string getName()
+    {
+        return _name;
+    }
 
 private:
     float _grade;
     std::string _name;
 };
 
-unsigned int randomInt(const size_t max)
+unsigned int randomInt(size_t max)
 {
-    /*Seed random number generator*/
-    std::srand (time(NULL));
+    static bool seeded;
+        if(seeded != true)
+        {
+            seeded = true;
 
-    return std::rand() % max;
+            /*Seed random number generator*/
+            std::srand (time(NULL));
+        }
+
+    return std::rand() % max; //Return a generate random int
+}
+
+void displayAllStudents(std::vector<Student> &students)
+{
+    for (int i = 0; i < students.size(); i++)
+    {
+        std::cout << students[i].getName() << ": " << students[i].getGrade() << "%" << std::endl;
+    }
+}
+
+void calculateStats(std::vector<Student> &students)
+{
+    /*Max and Min Pointers*/
+    Student* max = nullptr;
+    Student* min = nullptr;
+
+    /*Average grade*/
+    size_t average = 0;
+
+    /*Calculate Max and Min*/
+    for (int i = 0; i < students.size(); i++)
+    {
+        /*Get Sum of All Grades*/
+        average += students[i].getGrade();
+
+        /*Check for max*/
+        if(max = nullptr)
+        {
+            max = &students[i];
+        }
+        else if (students[i].getGrade() > max->getGrade()) //SEG FAULTS!!
+        {
+            max = &students[i];
+        }
+
+        /*Check for min*/
+        if(min = nullptr)
+        {
+            min = &students[i];
+        }
+        else if (students[i].getGrade() < min->getGrade())
+        {
+            min = &students[i];
+        }
+    }
+
+    /*Print max and min students*/
+    std::cout << "Maximum Grade:\n" << max->getName() << ": " << max->getGrade() << "%" << std::endl;
+    std::cout << "Minimum Grade:\n" << min->getName() << ": " << min->getGrade() << "%" << std::endl;
+
+    /*Calculate and Display Average*/
+    average /= students.size();
+    std::cout << "Average Grade: " << average << "%" << std::endl;
 }
 
 int main()
 {
+    /*Variables*/
     const size_t STUDENT_COUNT = 50;
 
     size_t choice; 
     size_t index;
     float grade;
+
 
     /*Students Vector*/
     std::vector<Student> students;
@@ -55,7 +126,7 @@ int main()
     /*Init Names vector*/
     std::vector<std::string> names;
 
-    std::fstream namesFile("nicknames.txt");
+    std::fstream namesFile("names.txt");
     std::string line;
 
     while (std::getline (namesFile, line))
@@ -69,7 +140,7 @@ int main()
     /*Main Menu Loop*/
     while (choice != 5)
     {
-        std::cout << "Main Menu: \n1.Display All Grade \n2.Randomize Students \n3.Stats \n4.Count Honours \n5.Exit\n";
+        std::cout << "Main Menu: \n1.Display All Grade \n2.Randomize Students \n3.Stats \n4.Count Honours \n5.Exit \nEnter menu selection (1-5): ";
         std::cin >> choice;
 
         switch (choice)
@@ -77,21 +148,28 @@ int main()
 
         /*Display all Grades*/
         case 1:
+            displayAllStudents(students);
             break;
 
         /*Randomize Students*/
         case 2:
+
+            /*Clear Students Vector*/
+            students.clear();
+
+            /*Generate New Students*/
             for (int i = 0; i < STUDENT_COUNT; i++)
             {
                 index = randomInt(names.size());
-                //grade = ???
+                grade = randomInt(100);
+
                 students.push_back(Student(names[index], grade));
-                // WORK HERE!!!!
             }
             break;
 
         /*Stats*/
         case 3:
+            calculateStats(students);
             break;
 
         /*Count Honours*/
